b96e469f6e3e7cc9690b7c9b710ddb80c3847e2e
jwakolbin <31264372+jwakolbin@users.noreply.github.com>
2017-10-17 00:41:27 -0400

    A3 4th Commit

--- a3/views/members.php
+++ a3/views/members.php
@@ -10,7 +10,9 @@ if( is_array($user) ) {
 <h1><?php echo $title;?></h1>
   </div>
 
-<?php echo $content;?>
+	<?php echo $user['email'];?>"><?php echo $user['email'];?></a></h3>
+	<p><?php echo $user['first_name'].$user['last_name'];?></p>
+	<p><?php echo $user['email'];?></p>
 
 
 </div>
@@ -26,7 +28,7 @@ if( is_array($user) ) {
 
 	<?php foreach($users as $p){?>
 		<h3><a href="<?php echo BASE_URL?>members/view/<?php echo $p['uID'];?>" Email="<?php echo $p['email'];?>"><?php echo $p['email'];?></a></h3>
-		<p><?php echo $p['first_name'];?><?php echo $p['last_name'];?></p>
+		<p><?php echo $p['first_name']." ".$p['last_name'];?></p>
 	<p><?php echo $p['email'];?></p>
 <?php }?>
 </div>
