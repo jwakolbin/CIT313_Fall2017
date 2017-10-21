32b5ace6edc95e7fda7a7144881d0e3802d91770
jwakolbin <31264372+jwakolbin@users.noreply.github.com>
2017-10-21 00:20:52 -0400

    E4 1st COMMIT

---
+++ ex4/application/controllers/logincontroller.php
@@ -0,0 +1,29 @@
+<?php
+
+class LoginController extends Controller{
+
+	protected $userObject;
+
+	 public function do_login() {
+		 //handle login
+
+		 $this->userObject = new User();
+
+		// var_dump($_POST);
+
+		if($this->userObject->checkUser($_POST['email'],$_POST['password'])) {
+
+			$userInfo = $this->userObject->getUserFromEmail($_POST['email']);
+
+			$_SESSION['uID'] = $userInfo['uID'];
+
+			header('Location: '.BASE_URL);
+		}
+		else {
+			$this->set('error','Wrong password/email combination');
+			//echo "Try Again";
+		}
+
+		}
+
+}
