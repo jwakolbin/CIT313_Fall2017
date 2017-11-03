2eb61e923487804f7e306838aa9c8b8a8009c37d
jwakolbin <31264372+jwakolbin@users.noreply.github.com>
2017-11-02 23:08:53 -0400

    ex 6 commit
    Ex 6

---
+++ ex6/application/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:22011/ex6/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'jwakolbi');
+define('DB_PASS', 'Jl8brary');
+define('DB_NAME', 'jwakolbi_db');
+
+define('REQFIELD', '<font color="#FF0000">*</font>');
+?>
