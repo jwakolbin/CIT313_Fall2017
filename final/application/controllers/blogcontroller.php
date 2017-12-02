6cd4147d071b65c91b18344869671f1005aa19bd
jwakolbin <31264372+jwakolbin@users.noreply.github.com>
2017-12-02 00:45:11 -0500

    final commit

---
+++ final/application/controllers/blogcontroller.php
@@ -0,0 +1,93 @@
+<?php
+
+class BlogController extends Controller{
+
+	public $postObject;
+	public $commentObject;
+
+
+   	public function post($pID){
+    $this->postObject = new Post();
+		$post = $this->postObject->getPost($pID);
+	  $this->set('post',$post);
+
+		$this->commentObject = new Comments();
+		$comments = $this->commentObject->getAllComments();
+		$this->set('comments',$comments);
+
+		$this->set('task', 'save');
+   	}
+
+		public function index(){
+		$this->postObject = new Post();
+		$posts = $this->postObject->getAllPosts();
+		$this->set('title', 'The Default Blog View');
+		$this->set('posts',$posts);
+		}
+
+/*
+		public function comment($commentID){
+    $this->postObject = new Comments();
+		$post = $this->postObject->getComment($commentID);
+	  $this->set('comment',$comment);
+   	}
+
+		public function comments(){
+		$this->commentObject = new Comments();
+		$comments = $this->commentObject->getAllComments();
+		$this->set('comments',$comments);
+		}
+		*/
+
+	public function add(){
+	$this->commentObject = new Comments();
+	$this->set('task', 'save');
+
+	$this->commentObject = new Comments();
+	$comments = $this->commentObject->getAllComments();
+	$this->set('comments',$comments);
+
+	$this->set('task', 'save');
+	}
+
+
+	public function save(){
+
+	$this->commentObject = new Comments();
+	$data = array('commentText'=>$_POST['commentText'], 'uID'=>$_POST['uID'],'postID'=>$_POST['postID'],'date'=>$_POST['date']);
+
+		//'date'=>$_POST[STR_TO_DATE('$date', '%m/%d/%Y');
+
+		$result = $this->commentObject->addComment($data);
+
+		$this->set('message', $result);
+		}
+
+		public function delete($commentID){
+			$this->commentObject = new Comment();
+			$post = $this->commentObject->getComment($commentID);
+
+			$this->set('commentID', $post['commentID']);
+			$this->set('commentText', $post['commentText']);
+			$this->set('uID', $post['uID']);
+			$this->set('postID', $post['postID']);
+			$this->set('date', $post['date']);
+			$this->set('task', 'update');
+			}
+
+			public function update(){
+				$data = array('commentText'=>$_POST['commentText'], 'uID'=>$_POST['uID'],'postID'=>$_POST['postID'],'date'=>$_POST['date'], 'commentId'=>$_POST['commentID']);
+
+				$this->postObject = new Post();
+
+				$result = $this->commentObject->updatePost($data);
+				$outcome = $this->commentObject->getAllComments();
+				$this->set('comments',$outcome);
+				$this->set('message', $result);
+				$this->set('task', 'update');
+			}
+
+
+}
+
+?>
---
+++ final/views/blog/add.php
@@ -0,0 +1,80 @@
+
+<div class="container">
+	<div class="page-header">
+
+  </div>
+
+  <?php if($message){?>
+    <div class="alert alert-success">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    	<?php echo $message?>
+    </div>
+  <?php }?>
+
+
+	<h3>Comments</h3>
+
+	<div class="container">
+			<div class="page-header">
+		<figure>
+			<?php foreach($comments as $c){?>
+				<p><?php echo $c['commentText']; ?></p>
+
+				<?php
+				if($u->isAdmin()) {
+				?>
+					<a href="<?php echo BASE_URL?>blog/delete/<?php echo $postID?>" class="btn btn-primary">Delete</a>
+				<?php
+				}
+
+				else {
+
+				}
+			}
+				?>
+
+				<br  />
+			</div>
+			</div>
+
+		</figure>
+
+  <h3>Add Comments</h3>
+  <?php
+  if($u->isRegistered()) {
+  ?>
+
+  <div class="row">
+      <div class="span8">
+
+        <form action="<?php echo BASE_URL?>blog/<?php echo $task?>/<?php echo $postID?>" method="post" onsubmit="editor.post()">
+          <input name="date" id="date" size="16" type="date" value="<?php echo $date = date('Y-m-d H:i:s'); ?>">
+          <br/>
+          <label>Comment Text</label>
+          <textarea name="commentText" style="width:556px;height: 200px"><?php echo $commentText; ?></textarea>
+          <label for="date">Date</label>
+          <?php // set timezone
+date_default_timezone_set('America/Indiana/Indianapolis');?>
+
+          <input type="hidden" name="postID" value="<?php echo $postID; ?>"/>
+
+          <input type="hidden" name="uID" value="<?php echo $uID; ?>"/>
+
+          <button id="submit" type="submit" class="btn btn-primary" >Submit</button>
+        </form>
+
+
+
+            <?php
+            }
+            else {
+
+            }
+            ?>
+
+          </div>
+          </div>
+
+
+</div>
+<?php include('views/elements/footer.php');?>
